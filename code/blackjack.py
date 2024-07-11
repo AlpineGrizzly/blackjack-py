95a329cb2dafafa7c5caa4fb6428f748140ea23f
r3v <r3v>
2024-07-11 14:41:15 -0700

    init

---
+++ code/blackjack.py
@@ -0,0 +1,42 @@
+'''
+blackjack.py
+
+implementation of the cardgame blackjack in python
+
+@author Dalton Kinney
+@date July 11th, 2024
+'''
+
+'''
+TO BE IMPLEMENTED
+- [ ] Allow for realistic drawing from a set 52 cards for the dealer and player
+- [ ] Allow for betting, player will lose game if they run out of money
+- [ ] Shuffling of deck after each deal
+- [ ] Allow for LAN multiplayer eventually?
+'''
+
+class Player:
+    '''
+    Player class
+    '''
+    pass
+
+class Game:
+    '''
+    Game class
+    '''
+    pass
+
+def main():
+    print("Welcome to blackjack-py")
+
+    # Initialize game and shuffle deck with random order
+
+    # Game Loop
+    # draw cards for the dealer and the player
+
+    # allow player to decide to hit or pass 
+
+
+if __name__ == "__main__":
+    main()
