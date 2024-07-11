4c6eaab1d0678a2420e38aecdcfeb436f2befac9
r3v <r3v>
2024-07-11 15:06:58 -0700

    setting the stage

--- code/blackjack.py
+++ code/blackjack.py
@@ -5,6 +5,8 @@ implementation of the cardgame blackjack in python
 
 @author Dalton Kinney
 @date July 11th, 2024
+
+blackjack resource https://bicyclecards.com/how-to-play/blackjack
 '''
 
 '''
@@ -13,6 +15,22 @@ TO BE IMPLEMENTED
 - [ ] Allow for betting, player will lose game if they run out of money
 - [ ] Shuffling of deck after each deal
 - [ ] Allow for LAN multiplayer eventually?
+
+Game specific
+- [ ] Init a shuffled deck
+- [ ] ability to place an initial bet with a starting amount
+- [ ] Given each player a card from the shuffled deck
+- [ ] dealer has one card face up and one facedown on second deal
+- [ ] if player has a natural black jack (21) and dealer does not, they gain 1.5x the bet amount
+- [ ] if dealer has natural and no one else does, collect from each player
+- [ ] if  player and dealer both have natural, player wins nothing and takes bakc chips
+
+Decision logic for player 
+- [ ] Player can stand or hit after two cards hit
+- [ ] 
+
+Decision logic for the Dealer
+- [ ] 
 '''
 
 class Player:
